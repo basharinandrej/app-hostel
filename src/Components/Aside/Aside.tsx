c6f47393de80a2cd26bff82727803bb8d3005a20
basharinandrej <basharin_andrej@mail.ru>
2021-09-30 10:56:45 +0300

    add component aside

---
+++ src/Components/Aside/Aside.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import './Aside.sass'
+
+import Form from "Components/Form/Form";
+import Input from "Components/UI/Input/Input";
+import Button from "Components/UI/Button/Button";
+
+
+const Aside = () => {
+    const [valueInputLocation, setValueInputLocation] = React.useState('')
+    const [isDirtyInputLocation, setIsDirtyInputLocation] = React.useState(false)
+    const [isValidInputLocation, setIsValidInputLocation] = React.useState(false)
+
+    const [valueInputTotalDay, setValueInputTotalDay] = React.useState('')
+    const [isDirtyInputTotalDay, setIsDirtyInputTotalDay] = React.useState(false)
+    const [isValidInputTotalDay, setIsValidInputTotalDay] = React.useState(false)
+
+    const [valueInputCheckInDay, setValueInputCheckInDay] = React.useState('')
+    const [isDirtyInputCheckInDay, setIsDirtyInputCheckInDay] = React.useState(false)
+    const [isValidInputCheckInDay, setIsValidInputCheckInDay] = React.useState(false)
+
+    const onChangeLocationHandler = (e: React.ChangeEvent) => {
+        const value = (e.target as HTMLInputElement).value
+        setIsDirtyInputLocation(true)
+        setValueInputLocation(value)
+        if (value) {
+            setIsValidInputLocation(true)
+        } else {
+            setIsValidInputLocation(false)
+        }
+    }
+    const onChangeTotalDayHandler = (e: React.ChangeEvent) => {
+        const value = (e.target as HTMLInputElement).value
+        setIsDirtyInputTotalDay(true)
+        setValueInputTotalDay(value)
+        if (value) {
+            setIsValidInputTotalDay(true)
+        } else {
+            setIsValidInputTotalDay(false)
+        }
+    }
+    const onChangeCheckInDayHandler = (e: React.ChangeEvent) => {
+        const value = (e.target as HTMLInputElement).value
+        setIsDirtyInputCheckInDay(true)
+        setValueInputCheckInDay(value)
+        if (value) {
+            setIsValidInputCheckInDay(true)
+        } else {
+            setIsValidInputCheckInDay(false)
+        }
+    }
+
+    return (
+        <aside className="layout__aside aside">
+            <Form additionalClassNames={['aside__main-form']}>
+                <Input
+                    legend="Локация"
+                    type="text"
+                    isRequired={true}
+                    value={valueInputLocation}
+                    isValid={isValidInputLocation}
+                    isDirty={isDirtyInputLocation}
+                    onChange={onChangeLocationHandler}
+                    additionalClassNames={['main-form__input', 'input--bold-legend']}
+                />
+                <Input
+                    legend="Дата заселения"
+                    type="date"
+                    isRequired={true}
+                    value={valueInputCheckInDay}
+                    isValid={isValidInputCheckInDay}
+                    isDirty={isDirtyInputCheckInDay}
+                    onChange={onChangeCheckInDayHandler}
+                    additionalClassNames={['main-form__input', 'input--bold-legend']}
+                />
+                <Input
+                    legend="Количество дней"
+                    type="number"
+                    isRequired={true}
+                    value={valueInputTotalDay}
+                    isValid={isValidInputTotalDay}
+                    isDirty={isDirtyInputTotalDay}
+                    onChange={onChangeTotalDayHandler}
+                    additionalClassNames={['main-form__input', 'input--bold-legend']}
+                />
+
+                <Button
+                    text="Войти"
+                    type="big"
+                />
+            </Form>
+        </aside>
+    )
+}
+
+export default Aside
